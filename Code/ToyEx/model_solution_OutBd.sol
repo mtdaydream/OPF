f48025c1d8001142e67505c7f163a1f403bd8dee
BenRapone <leiodes13@gmail.com>
2019-07-18 09:29:13 -0700

    Re-add ToyEx

---
+++ Code/ToyEx/model_solution_OutBd.sol
@@ -0,0 +1,22 @@
+# Solution for model OutBdModel
+# Objective value = 1.2499999999999998e+00
+x0 0
+x1 0
+x2 0
+x3 0
+x4 2
+x5 0
+x6 0
+x7 0
+x8 0.8
+x9 0
+x10 0
+x11 0.8
+x12 0
+x13 1
+x14 0
+x15 0
+C16 0
+C17 -1
+C18 0
+C19 1
